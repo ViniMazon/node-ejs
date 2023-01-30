b4d82d31073bb7157a3d1572008362a38d3c1d7d
ViniMazon <viniciusmazon100@gmail.com>
2023-01-30 17:25:50 -0300

    Node com EJS

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    const items = [
+        {title: "D", 
+        message: "esenvolver aplicações de forma fácil"
+        },
+        {
+            title: "E",
+            message: "JS usa Javascript para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        {
+            title:"A",
+            message: "morzinho"
+        },
+        {
+            title:"I",
+            message: "Install EJS"
+        },
+        {
+            title:"S",
+            message: "intaxe simples"
+        }
+    ];
+
+    const subtitle = "Uma linguagem de modelagem para criação de páginas HTML"
+    res.render("pages/index", {
+        qualitys: items, 
+        subtitle:subtitle,
+    });
+})
+
+app.get("/sobre", function(req, res){
+    res.render("pages/about");
+})
+
+app.listen(8080);
+console.log("Rodando");
